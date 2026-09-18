16e69bb0d3fce1336cd45c87d3a69d2b84277635
44311 <113581671+44311@users.noreply.github.com>
2026-09-18 15:26:26 +0800

    Add files via upload

---
+++ assets/models.js
@@ -0,0 +1,23 @@
+
+window.MODELS = {
+  cpu: {
+    name: "CPU / Processor",
+    file: "models/cpu.glb",
+    desc: "Contoh model prosesor 3D sederhana."
+  },
+  ram: {
+    name: "RAM",
+    file: "models/ram.glb",
+    desc: "Contoh modul memori RAM 3D sederhana."
+  },
+  gpu: {
+    name: "GPU / Kartu Grafis",
+    file: "models/gpu.glb",
+    desc: "Contoh kartu grafis 3D sederhana."
+  },
+  ssd: {
+    name: "SSD",
+    file: "models/ssd.glb",
+    desc: "Contoh media penyimpanan SSD 3D sederhana."
+  }
+};
